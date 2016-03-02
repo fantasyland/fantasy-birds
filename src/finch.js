843cc568ec3e170eb07467d1f7555cc6f06b9eff
Stoeffel <schtoeffel@gmail.com>
2016-03-02 11:07:21 +0100

    adds example for finch

--- src/finch.js
+++ src/finch.js
@@ -3,6 +3,12 @@ const curry = require('fantasy-helpers').curry
 
 //# finch :: a -> b -> (b -> a -> c) -> c
 //.
+//. F combinator
+//.
+//. ```js
+//. > finch(-1, 3)(x => y => x * y)
+//. -3
+//. ```
 const finch = curry((x, y, f) => f(y)(x))
 
 module.exports = finch
