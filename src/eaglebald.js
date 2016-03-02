acb5dd3b8dcbf3254686768ebed99820b83b12e4
Stoeffel <schtoeffel@gmail.com>
2016-03-02 11:05:39 +0100

    adds example to eaglebald

--- src/eaglebald.js
+++ src/eaglebald.js
@@ -3,6 +3,12 @@ const curry = require('fantasy-helpers').curry
 
 //# eaglebald :: (e -> f -> g) -> (a -> b -> e) -> a -> b -> (c -> d -> f) -> c -> d -> g
 //.
+//. E^ combinator
+//.
+//. ```js
+//. > eaglebald(x => y => y + x)(a => b => b + a)('!')('birds')(a => b => a + b)('fantasy')('-')
+//. 'fantasy-birds!'
+//. ```
 const eaglebald = curry((f, g, s, t, h, u, v) => f(g(s)(t))(h(u)(v)))
 
 module.exports = eaglebald
